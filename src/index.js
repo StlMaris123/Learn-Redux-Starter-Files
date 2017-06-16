cc3f89ecd54a45aa5161f007e13ac5f4aa53c18b
StlMaris123 <stellamarisnjage@gmail.com>
2017-06-16 12:45:54 +0300

    make App to be a class component instead of functional component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyCCiqulBqYtQgZq9h6OnFznxZIz_qDgvfg'
-const App =  () => {
-    return <div>
+
+YTSearch( { key: API_KEY, term: 'surfboards' }, function(data) {
+    console.log(data);
+});
+class App extends Component {
+    render(){
+        return(
+            <div>
                 <SearchBar />
-            </div>;
+            </div>
+        );
+
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
