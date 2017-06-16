4959c6049a06912a8c1e8902850a93732f2f5c2d
StlMaris123 <stellamarisnjage@gmail.com>
2017-06-16 12:51:11 +0300

    initialize state

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
 const API_KEY = 'AIzaSyCCiqulBqYtQgZq9h6OnFznxZIz_qDgvfg'
 
-YTSearch( { key: API_KEY, term: 'surfboards' }, function(data) {
-    console.log(data);
-});
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+    YTSearch( { key: API_KEY, term: 'surfboards' }, function(data) {
+    console.log(data);}
+});
+
     render(){
         return(
             <div>
